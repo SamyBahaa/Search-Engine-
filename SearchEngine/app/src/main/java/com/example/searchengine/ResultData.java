f8ce682ab27a236448777f0de8beee38f77ad576
Samy Bahaa <samyking.sb@gmail.com>
2020-05-23 16:32:09 +0200

    can not connect to local db and it give me time out

--- SearchEngine/app/src/main/java/com/example/searchengine/ResultData.java
+++ SearchEngine/app/src/main/java/com/example/searchengine/ResultData.java
@@ -7,5 +7,10 @@ public class ResultData {
     public String Name;
     public float Ranking;
 
-
+    public ResultData(String _id, String id, String name, float ranking) {
+        this._id = _id;
+        Id = id;
+        Name = name;
+        Ranking = ranking;
+    }
 }
--- SearchEngine/app/src/main/java/com/example/searchengine/SecondActivity.java
+++ SearchEngine/app/src/main/java/com/example/searchengine/SecondActivity.java
@@ -1,7 +1,10 @@
 package com.example.searchengine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -10,16 +13,46 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.SearchView;
 import android.widget.TextView;
 
+import com.mongodb.Block;
+import com.mongodb.Cursor;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+import org.json.JSONArray;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
 public class SecondActivity extends AppCompatActivity {
 
-    TextView test;
-
-
+    private TextView test;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private ArrayList<ResultData> mResultDataArrayList;
+    private JSONArray retArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         test =  findViewById(R.id.resultTextView) ;
+        FetchResult mFetchResult = new FetchResult();
+        mFetchResult.execute();
+        mRecyclerView= findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mResultDataArrayList = new ArrayList<>();
+        mAdapter = new ResultAdapter(mResultDataArrayList,this);
+        mRecyclerView.setAdapter(mAdapter);
+        retArray = new JSONArray();
+
     }
 
     @Override
@@ -47,5 +80,25 @@ public class SecondActivity extends AppCompatActivity {
         return true;
     }
 
+    private class FetchResult extends AsyncTask<String, String, String> {
 
+        @Override
+        protected String doInBackground(String... strings) {
+            getData();
+            return null;
+        }
+    }
+
+    public void getData(){
+//      MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        MongoClient mongoClient = new MongoClient("Localhost",27017);
+        MongoDatabase database =  mongoClient.getDatabase("result");
+        MongoCollection<Document> collection =  database.getCollection("result");
+       collection.find().forEach(new Block<Document>() {
+           @Override
+           public void apply(Document document) {
+               retArray.put(document);
+           }
+       });
+    }
 }
